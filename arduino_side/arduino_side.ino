e9b2e63daf74017c39a6e15854ac41778887cd03
Arion12112 <47907685+Arion12112@users.noreply.github.com>
2019-05-02 15:21:49 +0700

    Merapihkan dan menambah beberapa code

--- arduino_side/arduino_side.ino
+++ arduino_side/arduino_side.ino
@@ -1,41 +1,54 @@
 //arduino side
 #include <SoftwareSerial.h>
 
-
-SoftwareSerial s(10,11);//rx,tx
-//SoftwareSerial s2(12,13);
+//pilih sesuai dengan board yang dipakai
+//SoftwareSerial s(10,11);//rx,tx ;untuk arduino mega
+SoftwareSerial s(5,6); //rx, tx; untuk arduino uno
+//deklarasi variabel
 int data;
 int velocity;
 char data2[6];
 char data3[1];
 int command;
-void setup() {
+
+//memulai setup
+void setup() 
+{
+//mulai koneksi s ke nodemcu dengan baud rate 115200
 s.begin(115200);
-//s2.begin(115200);
 Serial.begin(9600);
 }
  
-void loop() {
+void loop() 
+{
 //data = command; 1 = maju, 2 = mundur, 3= kiri, 4 = kanan, 5 = stop
-
+  //listen ke port s dengan delay 2 second untuk menghindari error
   s.listen();
   delay(2);
+  //memulai penerimaan pesan ASCII yang difragmentasi dari nodemcu
   int i =0;
    while(s.available()>0)
   {
     data2[i]=s.read();
     i++;
-    
-    if(s.available()==0){
+  //mengubah pesan ASCII menjadi bentuk integer  
+   if(s.available()==0)
+   {
    data = atoi(data2);
+  //menerima topic dari pesan yang dikirim oleh nodemcu
    command = data%10;
-   
-   Serial.println(data);
-   data=0;
-  for(int i = 0;i<6;i++){
+  //menerima message dari pesan yang dikirim oleh nodemcu
+   velocity = floor(data/100); 
+   Serial.print("command : "); 
+   Serial.println(command);
+   Serial.print("velocity : "); 
+   Serial.println(velocity);
+  //meghapus memori yang digunakan di data dan di array saat menerima pesan
+  data=0;
+  for(int i = 0;i<6;i++)
+    {
     data2[i]=0;
     }
-      }
-
+   }
   }
 }
